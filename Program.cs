83df63a803b09c0c6c08e289e397fff7a8a62d19
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2023-03-21 12:22:44 +0530

    String Manipulations and comparisions

--- Program.cs
+++ Program.cs
@@ -4,9 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+           // Console.WriteLine("Hello");
             int a = 12;
-            Console.WriteLine(a); //From Visual Studio
+            //Console.WriteLine(a); //From Visual Studio
+            string b = "A";
+            string c = "a";
+            string d = "Test";
+            //Console.WriteLine(b.Substring(2)+" "+c.Substring(0,7)+" "+d);
+            Console.WriteLine(string.Concat(b,c));
+            Console.WriteLine(b.TrimEnd()+c);
+            Console.WriteLine(b.TrimEnd() + c.Trim());
+            Console.WriteLine("Comparision of 2 strings");
+            Console.WriteLine(string.Compare(b,c,StringComparison.Ordinal));
+
         }
     }
 }
